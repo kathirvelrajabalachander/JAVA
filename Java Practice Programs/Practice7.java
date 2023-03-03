1f87ffcbeb3cb0d1dbfdaa131d3bad2f1232e511
kathirvel <99076316+kathirvelrajabalachander@users.noreply.github.com>
2023-03-03 21:08:52 +0530

    practice problems - 3/3/2023
    completed

---
+++ Java Practice Programs/Practice7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Practice7{
+
+   public static void main(String[] args){
+
+     Scanner sc = new Scanner(System.in);
+     int a = sc.nextInt();
+     a = a/2;
+     for(int i=0;i<
+     System.out.println(a);
+   }
+}
